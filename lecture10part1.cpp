808600d277bb22e879d115cb4edc0976efbc1bc1
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-19 15:10:56 +0530

    Add files via upload

---
+++ lecture10part1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void PrintHi(){
+    cout<<"Hi"<<endl;
+    return ;
+}
+
+void sum(int a,int b){
+    int c=100;
+    cout<<a+b;
+}
+
+int main(){
+    
+    PrintHi();
+    int a=10;
+    int b=20;
+    int c;
+    cout<<c<<" sum is : ";
+    sum(a,b);
+    return 0;
+}
